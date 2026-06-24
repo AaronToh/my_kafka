23877fdb02cd2482e0a48948fc03e78c07ba4f74
aaron <aarontywei@gmail.com>
2026-06-24 17:08:31 +0800

    Bind

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include <sys/socket.h>
 
 int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 1);
 
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
     return 0;
 }
