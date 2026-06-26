9349f39a4852b60f3caab3026b461f8527155a5e
AaronToh <aarontywei@gmail.com>
2026-06-26 10:24:46 +0800

    Handle api versions

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Description
 
-My attempt at an implementatino of the kafka message queue.
+My attempt at an implementation of the kafka message queue.
+
+## Build
+
+```bash
+cmake -S . -B build && cmake --build build
+```
+
+## Run
+
+```bash
+./build/my_kafka
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,124 @@
-#include <cstdio>
+#include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+ssize_t recvAll(int socket, char* buffer, int32_t len) {
+    int32_t total = 0;
+    while (total < len) {
+        ssize_t n = recv(socket, buffer + total, len - total, 0);
+        if (n <= 0) return 0;
+        total += n;
+    }
+    return total;
+}
+
 int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 1);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Socket: " << strerror(errno) << "\n";
+        return 1;
+    }
 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(9092);
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // listen on all interfaces - wifi, IPv6
     
-    return 0;
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Bind: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Listen: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == -1) {
+        std::cerr << "Accept: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    while (true) {
+        char lenBuf[4];
+        if (recvAll(clientSocket, lenBuf, 4) <= 0) break;
+
+        int32_t msg_length;
+        memcpy(&msg_length, lenBuf, 4);
+        msg_length = ntohl(msg_length);
+
+        char msg[msg_length];
+        if (recvAll(clientSocket, msg, msg_length) <= 0) break;
+       
+        int16_t api_key, api_version, client_id_len;
+        int32_t correlation_id;
+
+        memcpy(&api_key, msg + 0, 2);
+        api_key = ntohs(api_key);
+        memcpy(&api_version, msg + 2, 2);
+        api_version = ntohs(api_version);
+        memcpy(&correlation_id, msg + 4, 4);
+        correlation_id = ntohl(correlation_id);
+        memcpy(&client_id_len, msg + 8, 2);
+        client_id_len = ntohs(client_id_len);
+        char client_id[client_id_len + 1];
+        memset(client_id, 0, client_id_len + 1);
+        if (client_id_len > 0) memcpy(client_id, msg + 10, client_id_len);
+
+        std::cout << "api_key: " << api_key << "\n";
+        std::cout << "api_version: " << api_version << "\n";
+        std::cout << "correlation_id: " << correlation_id << "\n";
+        std::cout << "client_id: " << client_id << "\n";
+
+        if (api_key == 18) {
+            if (api_version <= 2) {
+                char response[20];
+                int32_t resp_len = htonl(16);
+                memcpy(response, &resp_len, 4);
+                correlation_id = htonl(correlation_id);
+                memcpy(response + 4, &correlation_id, 4);
+                int16_t error = htons(0);
+                memcpy(response + 8, &error, 2);
+                int32_t arr_length = htonl(1);
+                memcpy(response + 10, &arr_length, 4);
+                int16_t api_key = htons(18);
+                memcpy(response + 14, &api_key, 2);
+                int16_t min_version = htons(0);
+                memcpy(response + 16, &min_version, 2);
+                int16_t max_version = htons(0);
+                memcpy(response + 18, &max_version, 2);
+                if (send(clientSocket, response, 20, 0) == -1) break;
+            } else {
+                char response[23];
+                int32_t resp_len = htonl(19);
+                memcpy(response, &resp_len, 4);
+                correlation_id = htonl(correlation_id);
+                memcpy(response + 4, &correlation_id, 4);
+                int16_t error = htons(0);
+                memcpy(response + 8, &error, 2);
+                uint8_t arr_length = 2;
+                memcpy(response + 10, &arr_length, 1);
+                int16_t api_key = htons(18);
+                memcpy(response + 11, &api_key, 2);
+                int16_t min_version = htons(0);
+                memcpy(response + 13, &min_version, 2);
+                int16_t max_version = htons(3);
+                memcpy(response + 15, &max_version, 2);
+                uint8_t entry_tags = 0;
+                memcpy(response + 17, &entry_tags, 1);
+                int32_t throttle_time_ms = htonl(0);
+                memcpy(response + 18, &throttle_time_ms, 4);
+                uint8_t resp_tags = 0;
+                memcpy(response + 22, &resp_tags, 1);
+                if (send(clientSocket, response, 23, 0) == -1) break;
+            }
+       } else {
+            break;
+       }
+    }
+       return 0;
 }
