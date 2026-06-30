3c0d098bad5e535a4e57905f8b220ae4e88e49d8
AaronToh <aarontywei@gmail.com>
2026-06-30 21:35:55 +0800

    Respond to Meta Request

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,92 @@ void closeConnection(int clientSocket, int epollfd, std::map<int, ConnectionStat
     connections.erase(clientSocket);
 }
 
+void sendApiVerResponse(int clientSocket, int32_t correlationId, int16_t apiVersion) {
+    if (apiVersion <= 2) {
+        char response[26];
+        char* p = response;
+        int32_t respLen = htonl(22);
+        memcpy(p, &respLen, 4); p += 4;
+        int32_t corrId = htonl(correlationId);
+        memcpy(p, &corrId, 4); p += 4;
+        int16_t error = htons(0);
+        memcpy(p, &error, 2); p += 2;
+        int32_t arrLength = htonl(2);
+        memcpy(p, &arrLength, 4); p += 4;
+        int16_t metaApiKey = htons(3);
+        memcpy(p, &metaApiKey, 2); p += 2;
+        int16_t metaMin = htons(0);
+        memcpy(p, &metaMin, 2); p += 2;
+        int16_t metaMax = htons(0);
+        memcpy(p, &metaMax, 2); p += 2;
+        int16_t apiVerKey = htons(18);
+        memcpy(p, &apiVerKey, 2); p += 2;
+        int16_t apiVerMin = htons(0);
+        memcpy(p, &apiVerMin, 2); p += 2;
+        int16_t apiVerMax = htons(0);
+        memcpy(p, &apiVerMax, 2); p += 2;
+        send(clientSocket, response, 26, 0);
+    } else {
+        // compact arrLength=3 means 2 entries: Metadata(3) and ApiVersions(18)
+        char response[30];
+        char* p = response;
+        int32_t respLen = htonl(26);
+        memcpy(p, &respLen, 4); p += 4;
+        int32_t corrId = htonl(correlationId);
+        memcpy(p, &corrId, 4); p += 4;
+        int16_t error = htons(0);
+        memcpy(p, &error, 2); p += 2;
+        uint8_t arrLength = 3;
+        memcpy(p, &arrLength, 1); p += 1;
+        int16_t metaApiKey = htons(3);
+        memcpy(p, &metaApiKey, 2); p += 2;
+        int16_t metaMin = htons(0);
+        memcpy(p, &metaMin, 2); p += 2;
+        int16_t metaMax = htons(0);
+        memcpy(p, &metaMax, 2); p += 2;
+        uint8_t metaTags = 0;
+        memcpy(p, &metaTags, 1); p += 1;
+        int16_t apiVerKey = htons(18);
+        memcpy(p, &apiVerKey, 2); p += 2;
+        int16_t apiVerMin = htons(0);
+        memcpy(p, &apiVerMin, 2); p += 2;
+        int16_t apiVerMax = htons(3);
+        memcpy(p, &apiVerMax, 2); p += 2;
+        uint8_t apiVerTags = 0;
+        memcpy(p, &apiVerTags, 1); p += 1;
+        int32_t throttleTimeMs = htonl(0);
+        memcpy(p, &throttleTimeMs, 4); p += 4;
+        uint8_t respTags = 0;
+        memcpy(p, &respTags, 1); p += 1;
+        send(clientSocket, response, 30, 0);
+    }
+}
+
+void sendMetadataResponse(int clientSocket, int32_t correlationId) {
+    // body: corrId(4) + brokers_count(4) + node_id(4) + host_len(2) + "localhost"(9) + port(4) + topics_count(4) = 31
+    char response[35];
+    char* p = response;
+    int32_t respLen = htonl(31);
+    memcpy(p, &respLen, 4); p += 4;
+    int32_t corrId = htonl(correlationId);
+    memcpy(p, &corrId, 4); p += 4;
+    int32_t brokerCount = htonl(1);
+    memcpy(p, &brokerCount, 4); p += 4;
+    int32_t nodeId = htonl(0);
+    memcpy(p, &nodeId, 4); p += 4;
+    int16_t hostLen = htons(9);
+    memcpy(p, &hostLen, 2); p += 2;
+    memcpy(p, "localhost", 9); p += 9;
+    int32_t port = htonl(9092);
+    memcpy(p, &port, 4); p += 4;
+    int32_t topicCount = htonl(0);
+    memcpy(p, &topicCount, 4); p += 4;
+    send(clientSocket, response, 35, 0);
+}
+
 void handleMessage(int clientSocket, int epollfd, std::map<int, ConnectionState>& connections, std::vector<char>& msgBuffer) {
+    std::cout << "handleMessage called\n";
+    std::flush(std::cout);
     int16_t apiKey, apiVersion, clientIdLen;
     int32_t correlationId;
 
@@ -45,47 +130,7 @@ void handleMessage(int clientSocket, int epollfd, std::map<int, ConnectionState>
     std::cout << "clientId: " << clientId << "\n";
 
     if (apiKey == 18) {
-        if (apiVersion <= 2) {
-            char response[20];
-            int32_t respLen = htonl(16);
-            memcpy(response, &respLen, 4);
-            int32_t corrId = htonl(correlationId);
-            memcpy(response + 4, &corrId, 4);
-            int16_t error = htons(0);
-            memcpy(response + 8, &error, 2);
-            int32_t arrLength = htonl(1);
-            memcpy(response + 10, &arrLength, 4);
-            int16_t respApiKey = htons(18);
-            memcpy(response + 14, &respApiKey, 2);
-            int16_t minVersion = htons(0);
-            memcpy(response + 16, &minVersion, 2);
-            int16_t maxVersion = htons(0);
-            memcpy(response + 18, &maxVersion, 2);
-            send(clientSocket, response, 20, 0);
-        } else {
-            char response[23];
-            int32_t respLen = htonl(19);
-            memcpy(response, &respLen, 4);
-            int32_t corrId = htonl(correlationId);
-            memcpy(response + 4, &corrId, 4);
-            int16_t error = htons(0);
-            memcpy(response + 8, &error, 2);
-            uint8_t arrLength = 2;
-            memcpy(response + 10, &arrLength, 1);
-            int16_t respApiKey = htons(18);
-            memcpy(response + 11, &respApiKey, 2);
-            int16_t minVersion = htons(0);
-            memcpy(response + 13, &minVersion, 2);
-            int16_t maxVersion = htons(3);
-            memcpy(response + 15, &maxVersion, 2);
-            uint8_t entryTags = 0;
-            memcpy(response + 17, &entryTags, 1);
-            int32_t throttleTimeMs = htonl(0);
-            memcpy(response + 18, &throttleTimeMs, 4);
-            uint8_t respTags = 0;
-            memcpy(response + 22, &respTags, 1);
-            send(clientSocket, response, 23, 0);
-        }
+        sendApiVerResponse(clientSocket, correlationId, apiVersion);
     } else if (apiKey == 3) {
         std::vector<std::string> names;
         if (apiVersion <= 2) {
@@ -106,7 +151,7 @@ void handleMessage(int clientSocket, int epollfd, std::map<int, ConnectionState>
                 reqBodyOff += nameLen;
             }
         } else {
-            uint8_t arrLength = msgBuffer[reqBodyOff++]; // assume length 1
+            uint8_t arrLength = msgBuffer[reqBodyOff++];
             arrLength--;
             names.resize(arrLength);
             for (int i = 0; i < arrLength; i++) {
@@ -120,12 +165,14 @@ void handleMessage(int clientSocket, int epollfd, std::map<int, ConnectionState>
                 reqBodyOff += 1; // tagged field
             }
         }
+        sendMetadataResponse(clientSocket, correlationId);
     } else {
         closeConnection(clientSocket, epollfd, connections);
     }
 }
 
 int main() {
+    setbuf(stdout, NULL);
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "Socket: " << strerror(errno) << "\n";
@@ -168,6 +215,7 @@ int main() {
 
     while (true) {
         epoll_event events[MAX_EVENTS];
+        std::cout << "waiting for events\n";
         int n = epoll_wait(epollfd, events, MAX_EVENTS, -1);
         if (n == -1) {
             std::cerr << "Wait: " << strerror(errno) << "\n";
@@ -193,28 +241,28 @@ int main() {
             } else {
                 int clientSocket = events[i].data.fd;
                 ConnectionState& state = connections[clientSocket];
+                std::cout << "event fd=" << clientSocket << " li=" << state.li << " mi=" << state.mi << "\n";
 
                 if (state.li < state.lenBuffer.size()) {
                     ssize_t bytesRead = recv(clientSocket, state.lenBuffer.data() + state.li, state.lenBuffer.size() - state.li, 0);
-                    if (bytesRead <= 0) {
-                        closeConnection(clientSocket, epollfd, connections);
-                        continue;
-                    }
+                    std::cout << "lenBuf bytesRead=" << bytesRead << " errno=" << errno << "\n";
+                    if (bytesRead == 0) { closeConnection(clientSocket, epollfd, connections); continue; }
+                    if (bytesRead < 0) { if (errno != EAGAIN) closeConnection(clientSocket, epollfd, connections); continue; }
                     state.li += bytesRead;
                     if (state.li == state.lenBuffer.size()) {
                         int32_t msgLength;
                         memcpy(&msgLength, state.lenBuffer.data(), 4);
                         msgLength = ntohl(msgLength);
+                        std::cout << "msgLength=" << msgLength << "\n";
                         state.msgBuffer.resize(msgLength);
                     }
                 }
 
                 if (state.li == state.lenBuffer.size() && !state.msgBuffer.empty()) {
                     ssize_t bytesRead = recv(clientSocket, state.msgBuffer.data() + state.mi, state.msgBuffer.size() - state.mi, 0);
-                    if (bytesRead <= 0) {
-                        closeConnection(clientSocket, epollfd, connections);
-                        continue;
-                    }
+                    std::cout << "msgBuf bytesRead=" << bytesRead << " errno=" << errno << "\n";
+                    if (bytesRead == 0) { closeConnection(clientSocket, epollfd, connections); continue; }
+                    if (bytesRead < 0) { if (errno != EAGAIN) closeConnection(clientSocket, epollfd, connections); continue; }
                     state.mi += bytesRead;
                     if (state.mi == state.msgBuffer.size()) {
                         handleMessage(clientSocket, epollfd, connections, state.msgBuffer);
